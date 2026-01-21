c66d93cbf74dd48e7267b6efe147f2e8c953304e
bidumcris <bidumcris@gmail.com>
2026-01-21 15:55:51 -0300

    Fix: redirect /favicon.ico

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # Algunos navegadores piden /favicon.ico sí o sí (aunque definamos <link rel="icon">)
+  # Redirigimos a un asset público estable.
+  get "/favicon.ico", to: redirect("/icon.png")
+
   # Root path
   root "home#index"
 
